199e82e80d0732c9810e6bbbfc2e57c98cd41174
BeatrizN <beatrizncsilveira@gmail.com>
2018-04-22 19:41:06 -0300

    Add files via upload

---
+++ integracao/src/main/java/br/ufg/inf/integracao/topico2/serializacao/atividade1/Produto.java
@@ -0,0 +1,29 @@
+package br.ufg.inf.integracao.topico2.serializacao.atividade1;
+/**
+ *
+ * @author Beatriz
+ */
+public class Produto {
+    public String nome;
+    public String descricao;   
+
+    public Produto(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setPreco(String descricao) {
+        this.descricao = descricao;
+    }
+}
