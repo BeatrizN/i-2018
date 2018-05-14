89c95dba6ee2567d660258ae25dc62eb00e35984
BeatrizN <beatrizncsilveira@gmail.com>
2018-05-14 20:53:54 -0300

    Update Questao4.java

--- topico1/src/main/java/br/ufg/inf/es/integracao/Questao4.java
+++ topico1/src/main/java/br/ufg/inf/es/integracao/Questao4.java
@@ -6,43 +6,52 @@
 
 package main.java.br.ufg.inf.es.integracao;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
- * Classe cujo primeiro argumento é o nome de um arquivo e cujo segundo argumento  é uma sequência de caracteres.
- * Quando executado, a sequência de caracteres fornecida como o segundo argumento é persistida no arquivo cujo
- * nome é fornecido como primeiro argumento. O texto deve ser persistido no formato UTF-8.
+ * Classe cujo primeiro argumento é o nome de um arquivo e cujo segundo
+ * argumento  é uma sequência de caracteres.
+ * Quando executado, a sequência de caracteres fornecida como o segundo
+ * argumento é persistida no arquivo cujo nome é fornecido como primeiro
+ * argumento. O texto deve ser persistido no formato UTF-8.
  */
 public class Questao4 {
 
+    /**
+     * O método o chama método "retornaNomeArquivo" e apresenta o resultado.
+     * @param args
+     */
     public static void main (String [] args) {
         try {
-            String a = retorna(args[0]);
-            System.out.println((String) args[0]);
-            System.out.println(args[1]);
+            retornaNomeArquivo(args[0], args[1]);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public static String retorna (String nomeArquivo) throws IOException {
+    /**
+     * O método verifica o nome do arquivo e o arquivo está vazio, caso não
+     * esteja ele imprime/exibe o nome do arquivo e o conteúdo do arquivo.
+     * @param nomeArquivo é o nome do arquivo
+     * @param arquivo parametro que recebe o conteúdo do arquivo.
+     * @throws IOException caso o nome do arquivo esteja vazio ou null o
+     * sistema apresenta excessão.
+     */
+    public static void retornaNomeArquivo (String nomeArquivo, String arquivo) throws IOException {
+
+        if (nomeArquivo.equals(null) || nomeArquivo.equals("")) {
+            throw new IllegalArgumentException("Arquivo inválido");
+        }
+
+        if(arquivo.equals(null) || arquivo.equals("")) {
+            throw  new  IllegalArgumentException("Arquivo inválido");
+        }
+
         PrintWriter pw = new PrintWriter(nomeArquivo, "UTF-8");
-        pw.write(nomeArquivo);
-        pw.write("\n");
+        pw.write(arquivo);
         pw.close();
 
-        Charset uft8 = Charset.forName("UTF-8");
-        Path arquivo = Paths.get(nomeArquivo);
-        System.out.println((String) nomeArquivo);
-        System.out.println(nomeArquivo);
-        String a = "Deve ser corrigido";
-
-        return a;
     }
 }
